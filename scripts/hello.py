5d32ce378fa1bfba50e7d64e77a47ab6c48e3e55
xpython21 <xpython21@gmail.com>
2024-06-01 01:43:56 +0200

    Modified hello script

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,1 +1,9 @@
-print("hello Alexander")
+import datetime
+
+# Get the current time
+now = datetime.datetime.now()
+
+# Print the current time in HH:MM:SS format
+print("Current Time =", now.strftime("%H:%M:%S")) # e.g. Current Time = 13:45:22
+
+print("Go to bed Alexander")
